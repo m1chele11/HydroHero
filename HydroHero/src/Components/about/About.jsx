d945796b867fe31e007fbfda0415f704727fd2d3
michele <mbarrant@iu.edu>
2024-07-18 13:23:30 -0400

    about page updates

--- HydroHero/src/Components/about/About.jsx
+++ HydroHero/src/Components/about/About.jsx
@@ -1,13 +1,48 @@
-//import React from 'react'
-
-
-
-
+// import React from 'react';
+import { Container, Typography, Box, Paper } from '@mui/material';
+import WaterDropTwoToneIcon from '@mui/icons-material/WaterDropTwoTone';
+import './about.css';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    
+    <Box className="about-section">
+      <Container maxWidth="md" className="about-content">
+        <Paper elevation={3} className="about-paper">
+          <Box display="flex" flexDirection="column" alignItems="center" p={3}>
+            <WaterDropTwoToneIcon color="primary" fontSize="large" />
+            <Typography variant="h4" component="h1" gutterBottom>
+              About HydroHero
+            </Typography>
+            <Typography variant="body1" component="p" gutterBottom>
+              HydroHero is your ultimate companion for tracking your water intake effortlessly. Our mission is to help you stay hydrated and maintain a healthy lifestyle by providing an easy-to-use and intuitive platform.
+            </Typography>
+            <Typography variant="body1" component="p" gutterBottom>
+              With HydroHero, you can set daily water intake goals, track your progress, and get reminders to drink water throughout the day. Join us on this journey to better hydration and health!
+            </Typography>
+          </Box>
+        </Paper>
+
+        <Paper elevation={3} className="about-paper">
+          <Box display="flex" flexDirection="column" alignItems="center" p={3}>
+            <WaterDropTwoToneIcon color="primary" fontSize="large" />
+            <Typography variant="h4" component="h1" gutterBottom>
+              About HydroHero
+            </Typography>
+            <Typography variant="body1" component="p" gutterBottom>
+              HydroHero is your ultimate companion for tracking your water intake effortlessly. Our mission is to help you stay hydrated and maintain a healthy lifestyle by providing an easy-to-use and intuitive platform.
+            </Typography>
+            <Typography variant="body1" component="p" gutterBottom>
+              With HydroHero, you can set daily water intake goals, track your progress, and get reminders to drink water throughout the day. Join us on this journey to better hydration and health!
+            </Typography>
+          </Box>
+        </Paper>
+
+      </Container>
+    </Box>
+
+    
+  );
 }
 
-export default About
+export default About;
--- HydroHero/src/Components/header/Header.jsx
+++ HydroHero/src/Components/header/Header.jsx
@@ -7,18 +7,19 @@ const Header = () => {
   return (
     <AppBar position="static" className="header">
       <Toolbar>
+
         <div className="header-left">
           <Typography variant="h6" component="div" className="title">
-            <div className='hydro'>
-              HydroHero
-            </div>
+          <Link to='/' className='nav-link'>HydroHero</Link>
           </Typography>
         </div>
+
         <div className='header-right'>
           <Link to='/' className='nav-link'>Home</Link>
           <Link to='/contact' className='nav-link'>Contact</Link>
           <Link to='/about' className='nav-link'>About</Link>
         </div>
+        
       </Toolbar>
     </AppBar>
   );
